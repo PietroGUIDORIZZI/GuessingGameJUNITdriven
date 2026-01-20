2977973fd670aa22f777524dfc90bfc9f2e080d6
Pietro Guidorizzi <peterguidorizzi@gmail.com>
2026-01-20 11:27:26 -0300

    Added @beforeEach annotation for garbage collector work.

--- src/test/com/guidorizzi/GuessingGameTest.java
+++ src/test/com/guidorizzi/GuessingGameTest.java
@@ -1,14 +1,22 @@
 package com.guidorizzi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GuessingGameTest {
 
+    private GuessingGame game;
+
+    @BeforeEach
+    void setUp() {
+        game = new GuessingGame();
+    }
+
+
     @Test
     public void testSimpleWinSituation(){
-        GuessingGame game = new GuessingGame();
         int randomNum = game.getRandomNumber();
         String message = game.guess(randomNum);
         assertEquals("You got it", message);
@@ -16,14 +24,12 @@ public class GuessingGameTest {
 
     @Test
     public void testOneWrongNegGuessSituation(){
-        GuessingGame game = new GuessingGame();
         String message = game.guess(-5);
         assertEquals("You didn't get it", message);
     }
 
     @Test
     public void testOneWrongPosGuessSituation(){
-        GuessingGame game = new GuessingGame();
         int randomNum = game.getRandomNumber();
         String message = game.guess(randomNum + 1);
         assertEquals("You didn't get it", message);
